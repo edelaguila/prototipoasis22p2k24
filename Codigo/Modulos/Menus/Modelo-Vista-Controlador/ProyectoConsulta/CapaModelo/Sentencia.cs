0788677617f314d07fbf4c299f19d4b7279bd272
JdanielS7 <jsierrac3@miumg.edu.gt>
2024-09-30 10:09:49 -0600

    COMPLETO

---
+++ Codigo/Modulos/Menus/Modelo-Vista-Controlador/ProyectoConsulta/CapaModelo/Sentencia.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/////////librerias////////
+using System.Data;
+using System.Data.Odbc;
+
+namespace CapaModelo
+{
+     public class Sentencia
+    {
+        Conexion con = new Conexion();
+        
+        // obtener datos de una tabla CAPA MODELO
+
+        public OdbcDataAdapter llenarTbl(string tabla)// metodo  que obtinene el contenio de una tabla
+        {
+            //string para almacenar los campos de OBTENERCAMPOS y utilizar el 1ro
+            string sql = "SELECT * FROM " + tabla + "  ;";
+            OdbcDataAdapter dataTable = new OdbcDataAdapter(sql, con.conexion());
+            return dataTable;
+        }
+    }
+}
