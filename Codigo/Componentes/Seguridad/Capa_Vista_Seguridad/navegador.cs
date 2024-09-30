a1de844e30f98cd911f1b90f5ef7109d8c47868f
EMERZON <egarciat16@miumg.edu.gt>
2024-09-30 09:54:28 -0600

    mantenimineto

---
+++ Codigo/Componentes/Seguridad/Capa_Vista_Seguridad/navegador.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Capa_Vista_Seguridad
+{
+    public partial class navegador : Form
+    {
+        public navegador(string idUsuario)
+        {
+            InitializeComponent();
+
+            string[] alias = { "codigo", "Nombre", "Puesto", "Depto", "estado" };
+            navegador1.AsignarAlias(alias);
+            navegador1.AsignarSalida(this);
+            navegador1.AsignarColorFondo(Color.CadetBlue);
+            navegador1.AsignarColorFuente(Color.Black);
+            navegador1.AsignarTabla("empleado");
+            navegador1.ObtenerIdAplicacion("1");
+            navegador1.ObtenerIdUsuario(idUsuario);
+            navegador1.AsignarAyuda("1");
+            navegador1.AsignarNombreForm("Mantenimiento examen");
+        }
+
+        private void navegador1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
