c145b781db3d9b2e70662fc9656890e31f56a5a9
jdcac <jcacaos1@miumg.edu.gt>
2024-09-30 09:45:58 -0600

    Examen parcial2 mantenimiento

---
+++ Prototipos/Mantenimientos/Prueba.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Capa_Vista_Seguridad
+{
+    public partial class Prueba : Form
+    {
+        public Prueba(string idUsuario)
+        {
+            InitializeComponent();
+
+            string[] alias = { "Codigo", "Nombre", "estado"};
+            navegador1.AsignarAlias(alias);
+            navegador1.AsignarSalida(this);
+            navegador1.AsignarColorFondo(Color.CadetBlue);
+            navegador1.AsignarColorFuente(Color.Black);
+            navegador1.ObtenerIdAplicacion("7777");
+            navegador1.AsignarAyuda("1");
+            navegador1.ObtenerIdUsuario(idUsuario);
+            navegador1.AsignarTabla("aulas");
+            navegador1.AsignarNombreForm("Aulas");
+        }
+    }
+}
