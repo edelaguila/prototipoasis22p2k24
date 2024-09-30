185ebcfb682175c774d9ef21519a0fbec3a2ee68
JvCas10 <jcastellanosp3@miumg.edu.gt>
2024-09-30 09:50:51 -0600

    Agregando mantenimiento a la tabla vendedores

--- Codigo/Componentes/Seguridad/Capa_Vista_Seguridad/MDI_Seguridad.cs
+++ Codigo/Componentes/Seguridad/Capa_Vista_Seguridad/MDI_Seguridad.cs
@@ -445,6 +445,13 @@ namespace Capa_Vista_Seguridad
 
         }
 
+        private void mantenimiento1ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Navegador Nav = new Navegador(idUsuario);
+            Nav.MdiParent = this;
+            Nav.Show();
+        }
+
         //------------------------------------------ Fin KATERYN DE LEON y Gabriela Suc ------------------------------------------
 
 
