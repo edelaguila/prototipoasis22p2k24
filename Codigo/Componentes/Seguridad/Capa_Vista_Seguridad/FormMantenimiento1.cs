98a078558140fd7937e5695428669dbc70c5d35c
Fernando García <fgarciad@miumg.edu.gt>
2024-09-30 09:54:15 -0600

    Parcial

---
+++ Codigo/Componentes/Seguridad/Capa_Vista_Seguridad/FormMantenimiento1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Capa_Vista_Seguridad
+{
+    public partial class FormMantenimiento1 : Form
+    {
+        public FormMantenimiento1(string idUsuario)
+        {
+            InitializeComponent();
+            string[] alias = { "IdEmpresa", "Nit", "Nombre", "Direccion", "Telefono",  "estado" };
+            navegador1.AsignarAlias(alias);
+            navegador1.AsignarSalida(this);
+            navegador1.AsignarColorFondo(Color.CadetBlue);
+            navegador1.AsignarColorFuente(Color.Black);
+            navegador1.AsignarTabla("empresa");
+            navegador1.ObtenerIdAplicacion("1000");
+            navegador1.ObtenerIdUsuario(idUsuario);
+            navegador1.AsignarAyuda("1");
+            navegador1.AsignarNombreForm("Empresa");
+        }
+    }
+}
